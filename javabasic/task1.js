5b4d15753f85089d3f9f935d020a2badb5cb4293
hassanahmedkk989-sketch <hassanahmedkk989@gmail.com>
2026-03-02 07:30:56 +0000

    data

---
+++ javabasic/task1.js
@@ -0,0 +1,33 @@
+//student info
+let StudentName="Hassan Ahmed Khan";
+let className =  " ssc part|| matriculation ";
+let RollNo = 101001  
+        //group
+       let Group= "pre-engeniraing";
+    //subject marks
+    let English=85;
+    let Chemistry=90;
+    let Physics=75;
+    let Math=30;
+    let Biology=99;
+    //totalmarks and percentage
+    let totalmarks=English+Chemistry+Physics+math+Biology
+    let percentage=(totalmarks/500)*100
+    
+
+    console.log("=====Marksheet=====");
+    cansole.log("Name:", StudentName)
+    cansole.log("class name:", className)
+    cansole.log("Roll No:" , RollNo)
+    
+
+    cansole.log("English:" , English)
+    cansole.log("Chemistry:" , Chemistry)
+    cansole.log("Physics:" , Physics)
+    cansole.log("Math:" , Math)
+    cansole.log("Biology:" , Biology)
+
+
+
+    cansole.log("Totalmarks:  ", totalmarks + "/500")
+    cansole.log("Percentage: " , percentage +"%" )
